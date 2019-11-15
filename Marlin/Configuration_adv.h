ed2f754f59555af886c3d8b1f2dc2fb31791c9b6
Simon Kong Win Chang <skullkill@skaccess.com>
2019-11-15 21:01:16 +0800

    change Z_CURRENT to 720 instead of 850
    change Z_CURRENT to 720 instead of 850 to decrease the stepper motor temp
    
    for reference
    850 66°C
    750 58°C
    720 51°C

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2001,7 +2001,7 @@
   #endif
 
   #if AXIS_IS_TMC(Z)
-    #define Z_CURRENT       850
+    #define Z_CURRENT       720
     #define Z_CURRENT_HOME  Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE          0.11
